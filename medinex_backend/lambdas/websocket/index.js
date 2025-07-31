0090d52cd0fe2a3b0b37383a216c0e81eda92705
SanjayKParida <sanjayp@flickit.app>
2025-07-31 13:32:40 +0530

    trash removal

--- medinex_backend/lambdas/websocket/index.js
+++
@@ -1,7 +0,0 @@
-import { connect } from "./connect.js";
-import { disconnect } from "./disconnect.js";
-import { sendMessage } from "./sendMessage.js";
-
-export const wsConnect = connect;
-export const wsDisconnect = disconnect;
-export const wsSendMessage = sendMessage;
